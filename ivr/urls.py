1d810b0c0e7af90a80b87dcc84dc4fa84c3e8857
ian mwangi <iankimotho233@gmail.com>
2020-04-20 15:53:43 +0300

    Create API path for language selection response in urls.py file

--- ivr/urls.py
+++ ivr/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Calls
+from .views import Calls, CallResponseForLanguage
 
 urlpatterns = [
     path('calls/', Calls.as_view(), name='calls'),
+    path('call/response/', CallResponseForLanguage.as_view(), name='language_response'),
 ]
