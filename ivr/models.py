a1baa35710153098997ce1846fdde1b785fbaa59
ian mwangi <iankimotho233@gmail.com>
2020-04-20 15:22:43 +0300

    Create the Session model

--- ivr/models.py
+++ ivr/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Session(models.Model):
+    session_id = models.CharField(max_length=50)
+    caller_number = models.CharField(max_length=20)
+    dtmfDigits = models.CharField(max_length=10)
+    recordingUrl = models.CharField(max_length=100)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.caller_number
+
