83838d1b77d4f354f83c76877d6377e2e075a1b8
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-04-21 22:51:42 +0300

    Created userSerializer in serializers file

--- ivr/serializers.py
+++ ivr/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import UserBankDetails
+from .models import User,UserBankDetails
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=('user_id','first_name','last_name','phone_no')
 
 class UserBankDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model=UserBankDetails
         fields=('user_id','account_number','deposit','withdrawal','account_balance')
+
